649f46a3042ed18c423df1cd41ff1e7d69547e91
Denis Korekov <321den@gmail.com>
2015-04-09 15:07:20 +0200

    Adds Basket panel class

---
+++ src/View/Panels/BasketScreen.java
@@ -0,0 +1,12 @@
+package View.Panels;
+
+import javax.swing.*;
+
+/**
+ * Created by Denis on 9.4.2015.
+ */
+public class BasketScreen extends JPanel{
+    //should display all trips that are in basket with their price
+    //whenever you pay trips are getting out of basket
+    //should add "pay" and "back to browsing" and "User start screen" buttons
+}
